d15b972e0e21fde6ea0712dfabe68da1d7a05bf4
guofeng <17801196234@163.com>
2018-01-06 17:56:39 +0800

    img

--- src/components/usercenter/download.vue
+++ src/components/usercenter/download.vue
@@ -3,18 +3,20 @@
     <h3>我的头像</h3>
     <div class="download" v-show="fix">
       <img class="now" :src="baseurl+userurl">
+      <el-button plain class="btnright"  type="success" @click="fixchange(false)">修改头像</el-button>
     </div>
-    <div class="download"  >
-      <el-upload
+    <div class="download"  v-show="fix==false">
+      <el-upload class="now userimg"
         action="https://jsonplaceholder.typicode.com/posts/"
         list-type="picture-card"
         :on-preview="handlePictureCardPreview"
         :on-remove="handleRemove">
         <i class="el-icon-plus"></i>
       </el-upload>
-      <el-dialog :visible.sync="dialogVisible" size="tiny">
+      <el-dialog :visible.sync="dialogVisible" size="tiny" class="userimg" >
         <img width="100%" :src="dialogImageUrl" alt="">
       </el-dialog>
+      <el-button plain class="btnleft"  type="warning" @click="fixchange(true)">取消修改</el-button>
     </div>
   </div>
 </template>
@@ -41,6 +43,9 @@ export default {
     }
   },
   methods: {
+      fixchange(a){
+        this.fix=a;
+      },
       handleRemove(file, fileList) {
         console.log(file, fileList);
       },
@@ -69,13 +74,30 @@ export default {
     background: #fdfdfd;
     border:1px solid #f2f2f2;
     border-radius: 6px;
+    position: relative;
   }
 	.now{
     width: 148px;
-    margin-left: 100px;
+    margin-left: 180px;
     margin-top: 80px;
     height: 148px;
     border-radius: 6px;
+    display: block;
+    float: left;
     border: 1px solid #f5f5f5;
+  }
+  .btnright{
+    margin-top: 140px;
+    margin-left: 100px;
+  }
+  .userimg{
+    position: absolute;
+    top: 0px;
+    left: 0;
+
+  }
+  .btnleft{
+    margin-top: 140px;
+    margin-left: 440px;
   }
 </style>
